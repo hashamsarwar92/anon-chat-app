99de36d1d21d78c2750378f330cba0363fedd993
Hasham Sarwar <hsarwar4227@gmail.com>
2025-10-14 20:35:12 +0100

    Add User and Message mongoose schemas and Zod validation for signup, signin, verify, accept messages and messages

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+export const messageSchema = z.object({
+    content: z.string()
+        .min(1, { message: "Message content cannot be empty" })
+        .max(500, { message: "Message content cannot exceed 500 characters" })
+});
