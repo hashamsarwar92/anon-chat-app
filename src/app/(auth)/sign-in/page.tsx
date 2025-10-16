4205183cd84b182495821a736c29526c182d41e5
Hasham Sarwar <hsarwar4227@gmail.com>
2025-10-16 12:59:45 +0100

    feat(auth): install NextAuth and add sign-in backend- Installed and configured NextAuth- Implemented sign-in API route using CredentialsProvider- Added middleware for route protection- Set up AuthProvider for NextAuth session context- Updated NextAuth User and Session interfaces for custom fields- Created basic sign-in page (no design yet)

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import {useSession, signIn, signOut} from "next-auth/react"
+
+export default function Component (){
+    const {data: session} = useSession();
+    if(session){
+        return(<>
+        Signed in as {session.user?.email} <br/>
+        <button onClick={() => signOut()}>Sign out</button>
+        </>)
+    }
+    return(<>
+        Signed not <br/>
+        <button className="bg-orange-500 px-3 py-2 m-4 rounded" onClick={() => signIn()}>Sign In</button>
+        </>)
+}
