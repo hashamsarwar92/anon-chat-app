da8e7875a30a0ef1c7a7ed90cf0dd4b82d87cc3c
Hasham Sarwar <hsarwar4227@gmail.com>
2025-10-18 13:40:10 +0100

    Add accept message API (POST to update status, GET to retrieve status) and get message API (not tested yet)

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,37 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import mongoose from "mongoose";
+import UserModel from "@/models/User";
+import { ca } from "zod/locales";
+
+
+export async function GET(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    const user: User = session?.user as User;
+    if(!session || !session.user) {
+        return Response.json({ status: false, message: "Unauthorized" }, { status: 401 });
+    }
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try {
+        const user = await UserModel.aggregate([
+            { $match: { _id: userId } },
+            { $unwind: "$messages" },
+            { $sort: { "messages.createdAt": -1 } },
+            { $group: {
+                _id: "$_id",
+                messages: { $push: "$messages" }
+            } }
+        ])
+
+        if(!user || user.length === 0) {
+            return Response.json({ status: false, message: "User not found" }, { status: 404 });
+        }
+        return Response.json({ status: true, messages: user[0].messages }, { status: 200 });
+    }catch (error) {
+        console.error("Error fetching messages:", error);
+        return Response.json({ status: false, message: "Internal Server Error" }, { status: 500 });
+    }
+}
